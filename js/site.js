ea470bf992d0366f6cecf8060a04fe991d8e89a7
mrkinsey <mrkinsey10@gmail.com>
2023-08-06 19:49:11 -0400

    Rewind v1 completed

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-// Call Hello World
-function helloWorld(){
-    alert("Hello World!");
+// Get the string from the user
+// Controller function
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+// Reverse the string
+// Logic function
+function reverseString(userString) {
+    let revString = [];
+
+    for (let i = userString.length -1 ; i >= 0 ; i--) {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// Display the reversed string to the user
+// View function
+function displayString(revString) {
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    
+    document.getElementById("alert").classList.remove("invisible");
 }
